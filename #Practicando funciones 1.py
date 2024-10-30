6036b3c1dc438a7e467765f94f80dc2525ee143d
ange960 <a.23308051281196@cbtis128.edu.mx>
2024-10-30 15:09:30 -0600

    Add files via upload

---
+++ #Practicando funciones 1.py
@@ -0,0 +1,10 @@
+#Practicando funciones
+#1- En Python una funcion es definida usando la palabra def como palabra clave 
+#2- Para llamar a una funcion, use la funcion nombrada y seguida de parentesis:
+print("")# imprime espacio en blanco 
+print("Martinez Tagle Luis Angel 3w 1196 ")#imprime el nombre del programador 
+print("")#imprime espacio en blanco 
+def my_function():
+    print ("Hola Desde una funcion ")
+
+my_function()
