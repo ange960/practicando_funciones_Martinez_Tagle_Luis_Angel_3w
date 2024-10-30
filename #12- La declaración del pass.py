b28b85ab847847c3bb18e99448817c0429d65b46
ange960 <a.23308051281196@cbtis128.edu.mx>
2024-10-30 16:55:52 -0600

    Add files via upload

---
+++ #12- La declaración del pass.py
@@ -0,0 +1,15 @@
+#12- La declaración del pass
+#Las definiciones de función no pueden estar vacías, pero si por alguna razón tiene una definición de función sin contenido, ingrese la instrucción pass para evitar recibir un error.
+# Mensaje explicativo
+print("")#espacio en blanco 
+print("Martinez Tagle Luis Angel 3w 1196 ") #imprime el nombre del programador
+print("")#espacio en blanco 
+
+print("Pass funciona para ignorar si no hay línea de código y este se ejecute sin error.")
+
+def myfunction():
+    pass  
+
+myfunction()
+
+print("La función 'myfunction' se ejecutó, aunque no hace nada por ahora.")
