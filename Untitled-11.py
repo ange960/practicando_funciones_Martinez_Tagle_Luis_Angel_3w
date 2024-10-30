cd5ebb4af39568bfd8dcd9977cb6801397a5ece0
ange960 <a.23308051281196@cbtis128.edu.mx>
2024-10-30 16:55:37 -0600

    Add files via upload

---
+++ Untitled-11.py
@@ -0,0 +1,13 @@
+print("")#espacio en blanco 
+print("Martinez Tagle Luis Angel 3w 1196 ") #imprime el nombre del programador
+print("")#espacio en blanco 
+
+# Definimos la función con un parámetro
+def my_function(x):
+    # Retorna el valor de x multiplicado por 2
+    return x * 2
+
+# Llamamos a la función y mostramos el resultado
+print(my_function(3))  # Esto imprimirá 6
+print(my_function(5))  # Esto imprimirá 10
+print(my_function(9))  # Esto imprimirá 18
